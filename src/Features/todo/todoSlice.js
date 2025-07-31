aff0748c15bf73dbe79e1eaaa22833814df2066d
amitpal603-ops <ap036776@gmail.com>
2025-07-31 11:21:35 +0530

    modify

--- src/Features/todo/todoSlice.js
+++ src/Features/todo/todoSlice.js
@@ -3,26 +3,30 @@ import { createSlice, nanoid } from '@reduxjs/toolkit';
 export const TodoSlice = createSlice({
   name: 'todo',
   initialState: {
-    todos: [{ id: 1, task: "Add Another task" }]
+    todos: [{ id: 1, task: "Add Another task", completed: false }]
   },
   reducers: {
     addTodo: (state, action) => {
-      const todo = { id: nanoid(), task: action.payload };
+      const todo = { id: nanoid(), task: action.payload, completed: false };
       state.todos.push(todo);
     },
     removetodo: (state, action) => {
       state.todos = state.todos.filter(todo => todo.id !== action.payload);
     },
     updataTodo: (state, action) => {
-     const {id,task} = action.payload
-     const todo = state.todos.find((todo) => todo.id === id)
-
-     if(todo){
-      todo.task = task
-     }
+      const todo = state.todos.find((todo) => todo.id === action.payload.id);
+      if (todo) {
+        todo.task = action.payload.task;
+      }
+    },
+    complate: (state, action) => {
+      const todo = state.todos.find((todo) => todo.id === action.payload.id);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
     }
   }
 });
 
-export const { addTodo, removetodo, updataTodo } = TodoSlice.actions;
+export const { addTodo, removetodo, updataTodo, complate } = TodoSlice.actions;
 export default TodoSlice.reducer;
--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { removetodo, updataTodo } from '../Features/todo/todoSlice';
+import { removetodo, updataTodo,complate } from '../Features/todo/todoSlice';
 import { RiDeleteBin6Fill } from "react-icons/ri";
 import { CiEdit } from "react-icons/ci";
 import { LuSaveAll } from "react-icons/lu";
+import { GiCheckMark } from "react-icons/gi";
 
 function TodosList() {
   const todos = useSelector(state => state.todos);
@@ -11,6 +12,7 @@ function TodosList() {
 
     const [EditId ,setEditId] = useState(null)
     const [EditTaskId, setTaskId] = useState('')
+
 
     const HandleEdit = (todo) => {
         setEditId(todo.id)
@@ -22,7 +24,9 @@ function TodosList() {
       setEditId(null)
       setTaskId('')
     } 
- 
+    const HandleCheck = (todo) => {
+          dispatch(complate({id: todo.id}))
+    }
 
   return (
     <div className='mx-auto max-w-xl '>
@@ -35,15 +39,22 @@ function TodosList() {
               className='shadow-lg px-4 py-3 flex justify-between bg-gradient-to-r from-white-500 to-purple-600 text-white rounded-md transition duration-75'
             >
                 
-             <div className='flex gap-4'>
-              <input type="checkbox" name="" id="" />
+          <div className='flex gap-4 justify-center items-center'>
+  
+            <button
+              onClick={() => HandleCheck(todo)}
+              className={`h-4 w-4 rounded-full  border-2 border-black flex items-center justify-center ${
+              todo.completed ? 'bg-green-400' : ''
+                  }`}>
+               {todo.completed && <GiCheckMark />}
+            </button>
                {EditId === todo.id ? (
                 <input 
                 value={EditTaskId}
                 onChange={(e) => setTaskId(e.target.value)}
                 type="text" placeholder={todo.task} />
                ) : (
-                <span className=' text-lg'>{todo.task}</span>
+                <span  className='text-lg'>{todo.task}</span>
               )}
              </div>
 
